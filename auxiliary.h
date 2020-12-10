c0beffc385533650e0854f2cf29c2c684ca6de07
DBarinovv <9653314755@bk.ru>
2020-12-10 22:23:44 +0300

    Draw with click

--- auxiliary.h
+++ auxiliary.h
@@ -30,6 +30,7 @@ struct Coord_t
 {
     Coord_t (const int x = 0, const int y = 0) : x (x), y (y) {}
     Coord_t (POINT point) : x (point.x), y (point.y) {}
+    operator POINT () { return {x, y}; }
 
     Coord_t            (const Coord_t& other) = default;
     Coord_t& operator= (const Coord_t& other) = default;
